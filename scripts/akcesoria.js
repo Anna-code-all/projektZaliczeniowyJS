3eb7057881cc8af93f9d8eb544c25d033a2f0154
Anna-code-all <anna.bielicka1987@gmail.com>
2024-05-16 13:35:53 +0200

    osobne pliki

---
+++ scripts/akcesoria.js
@@ -0,0 +1,25 @@
+const akcesoria = [
+  {
+    poz: 1,
+    obraz: "assets/opony.jpg",
+    nazwa: "Opony z felgami",
+    marka: "Michelin",
+    cena: 10000,
+  },
+  {
+    poz: 2,
+    obraz: "assets/kamerka.jpg",
+    nazwa: "Kamerka samochodowa",
+    marka: "Sony",
+    cena: 1200,
+  },
+  {
+    poz: 3,
+    obraz: "assets/ubezpieczenie.avif",
+    nazwa: "Ubezpiecznenie na rok",
+    marka: "PZU",
+    cena: 8000,
+  },
+];
+
+export default akcesoria;
